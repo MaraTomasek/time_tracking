ecf422ba66cf8f5d93f8ba834e6522d662e8cf1a
Mara Tomasek <mara.tomasek@gmx.de>
2025-01-20 20:07:32 +0100

    Initial Commit

---
+++ src/test/java/com/interview/time_tracking/TimeTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.time_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
